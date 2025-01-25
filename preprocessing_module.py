504b75432910df089f77856a2595632e30f8c67d
Gabriel Martini <martinigabriel@Live.com>
2025-01-25 17:23:10 -0300

    1

--- preprocessing_module.py
+++ preprocessing_module.py
@@ -1,3 +1,13 @@
 def my_function():
     print("Hello from my_function in my_module!")
     return 1
+
+def installModules():
+    !pip install mysql-connector-python
+    !pip install pymysql
+
+    import pymysql
+    import pandas as pd
+
+    #Exibir todas as colunas ao visualizar dataframes
+    pd.set_option('display.max_columns', None)
