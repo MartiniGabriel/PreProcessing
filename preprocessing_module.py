e7db6d6b1662d28bb9933bd44232c6f858048993
Gabriel Martini <martinigabriel@Live.com>
2025-01-25 22:45:56 -0300

    a

--- preprocessing_module.py
+++ preprocessing_module.py
@@ -1,9 +1,6 @@
 import pymysql
 import pandas as pd
 
-def my_function():
-    print("Hello from my_function in my_module!")
-    return 1
 
 def installModules():
     import pymysql
@@ -49,3 +46,84 @@ def fetch_data_in_batches(query, MYSQL_HOST, MYSQL_PORT, MYSQL_USERNAME, MYSQL_P
         if connection:
             connection.close()
             print("Conexão fechada.")
+
+
+def clean_data(df):
+    df.isnull().sum() + (df == '').sum()
+
+    #Removendo as instâncias nulas e em branco
+    df = df.dropna()
+    df = df[(df != '').all(axis=1)]
+
+    #Validando a quantidade de nulos após a exclusão
+    df.isnull().sum() + (df == '').sum()
+    return df
+
+def fix_data_types(df):
+    df['Leucócitos'] = df['Leucócitos'].astype(float)
+    df['Mielócitos'] = df['Mielócitos'].astype(float)
+    df['Metamielócitos'] = df['Metamielócitos'].astype(float)
+    df['Bastões'] = df['Bastões'].astype(float)
+    df['Segmentados'] = df['Segmentados'].astype(float)
+    df['Eosinófilos'] = df['Eosinófilos'].astype(float)
+    df['Basófilos'] = df['Basófilos'].astype(float)
+    df['Linfócitos'] = df['Linfócitos'].astype(float)
+    df['Linfócitos Atípicos'] = df['Linfócitos Atípicos'].astype(float)
+    df['Monócitos'] = df['Monócitos'].astype(float)
+    df['Plasmócitos'] = df['Plasmócitos'].astype(float)
+    df['Blastos'] = df['Blastos'].astype(float)
+    df['Eritrócitos'] = df['Eritrócitos'].astype(float)
+    df['Hemoglobina'] = df['Hemoglobina'].astype(float)
+    df['Hematócrito'] = df['Hematócrito'].astype(float)
+    df['HCM'] = df['HCM'].astype(float)
+    df['CHCM'] = df['CHCM'].astype(float)
+    df['RDW'] = df['RDW'].astype(float)
+    df['Plaquetas'] = df['Plaquetas'].astype(float)
+    df['MVP'] = df['MVP'].astype(float)
+    df['Promielócitos'] = df['Promielócitos'].astype(float)
+    df['A1C'] = df['A1C'].astype(float)
+    df['Classe A1c'] = df['Classe A1c'].astype(int)
+    df['Classe A1c2'] = df['Classe A1c2'].astype(int)
+    return df
+
+def remove_unusual_variables(df):
+    df.drop('CodigoOs', axis=1, inplace=True)
+    df.drop('Data Nascimento', axis=1, inplace=True)
+    df.drop('Classe Idade', axis=1, inplace=True)
+    df.drop('HCM_1', axis=1, inplace=True)
+    df.drop('GME', axis=1, inplace=True)
+    df.drop('G', axis=1, inplace=True)
+    df.drop('Paciente', axis=1, inplace=True)
+    df.drop('Data Cadastro', axis=1, inplace=True)
+    df.drop('Data Cadastro Date', axis=1, inplace=True)
+    df.drop('Data Nascimento Data', axis=1, inplace=True)
+    return df
+
+def remove_outliers(df):
+    df = df[df['Leucócitos'] <= 200000]
+    df = df[df['Mielócitos'] <= 2000]
+    df = df[df['Metamielócitos'] <= 4000]
+    df = df[df['Segmentados'] <= 50000]
+    df = df[df['Eosinófilos'] <= 25000]
+    df = df[df['Basófilos'] <= 400]
+    df = df[df['Linfócitos'] <= 100000]
+    df = df[df['Linfócitos Atípicos'] <= 1700]
+    df = df[df['Monócitos'] <= 10000]
+    df = df[df['Plasmócitos'] <= 1000]
+    df = df[df['Blastos'] <= 50000]
+    df = df[df['Eritrócitos'] <= 8.8]
+    df = df[df['Hemoglobina'] >= 3]
+    df = df[df['HCM'] <= 140]
+    df = df[df['CHCM'] <= 38]
+    df = df[df['MVP'] >= 3]
+    df = df[df['Plaquetas'] <= 1300]
+    return df
+
+def get_data(query, MYSQL_HOST, MYSQL_PORT, MYSQL_USERNAME, MYSQL_PASSWORD, DB_NAME):
+    installModules()
+    df = fetch_data_in_batches(query, MYSQL_HOST, MYSQL_PORT, MYSQL_USERNAME, MYSQL_PASSWORD, DB_NAME)
+    df = clean_data(df)
+    df = fix_data_types(df)
+    df = remove_unusual_variables(df)
+    df = remove_outliers(df)
+    return df
