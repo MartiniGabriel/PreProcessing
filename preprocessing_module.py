78f9974d6d416671272a46f2ef7d8f21595d7c1a
Gabriel Martini <martinigabriel@Live.com>
2025-02-10 13:22:22 -0300

    adjust on y_train

--- preprocessing_module.py
+++ preprocessing_module.py
@@ -146,7 +146,7 @@ def get_train_data(query, MYSQL_HOST, MYSQL_PORT, MYSQL_USERNAME, MYSQL_PASSWORD
     df = remove_unusual_variables(df)
     df = remove_outliers(df)
     X_train, _, y_train, _ = createSubsets(df)
-    X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2, random_state=42, stratify=y)
+    X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2, random_state=42, stratify=y_train)
     return X_train, X_test, y_train, y_test
 
 def get_raw_data(query, MYSQL_HOST, MYSQL_PORT, MYSQL_USERNAME, MYSQL_PASSWORD, DB_NAME):
